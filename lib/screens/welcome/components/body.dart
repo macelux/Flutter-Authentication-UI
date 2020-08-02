78db1a4a88c1eca82f98b855df38f711a0e1105e
Macelux <macelux@hotmail.com>
2020-08-02 23:56:52 +0100

    first commit

---
+++ lib/screens/welcome/components/body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import './background.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { 
+   
+    return Background(child: Column(
+      children: <Widget>[],
+    ),);
+  }
+}
+
+ 
